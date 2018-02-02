4502fcc7bf45c85ebd2724a2c47c1bfa1c7c1932
Martin Otyeka <otyeka.martin@gmail.com>
2018-02-02 13:51:05 -0500

    Setups VCs

--- Tabman Style/MainViewController.swift
+++ Tabman Style/MainViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import Tabman
+import Pageboy
 
-class MainViewController: UIViewController {
-
+class MainViewController: TabmanViewController {
+    
+    var viewControllers: [UIViewController] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadViewControllers()
+        self.dataSource = self
     }
-
+    
 }
 
+extension MainViewController: PageboyViewControllerDataSource {
+    
+    func loadViewControllers() {
+        
+        for index in 0...4 {
+            let viewController = UIViewController()
+            switch index {
+            case 0:
+                viewController.title = "One"
+                viewController.view.backgroundColor = .red
+            case 1:
+                viewController.title = "Two"
+                viewController.view.backgroundColor = .blue
+            case 2:
+                viewController.title = "Three"
+                viewController.view.backgroundColor = .cyan
+            case 3:
+                viewController.title = "Four"
+                viewController.view.backgroundColor = .yellow
+            case 4:
+                viewController.title = "Five"
+                viewController.view.backgroundColor = .purple
+            default:
+                break
+            }
+            self.viewControllers.append(viewController)
+        }
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        self.bar.items = self.viewControllers.map { Item(title: $0.title ?? "") }
+        return self.viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return self.viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return Page.at(index: 2)
+    }
+    
+    
+}
